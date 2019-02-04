f71bae2a7fb351fc4880ca0fe13861fbb5cc5637
rorogarcete <oneosmar89@gmail.com>
2019-02-04 09:51:05 -0300

    Add assets

---
+++ app/src/main/java/com/vortigo/pokemonfinder/ui/main/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.vortigo.pokemonfinder.ui.main
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.vortigo.pokemonfinder.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
