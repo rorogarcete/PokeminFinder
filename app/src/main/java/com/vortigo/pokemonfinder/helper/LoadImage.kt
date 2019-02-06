f185ec80b972d581ac1225eb8ccae12ec96f5b82
rorogarcete <oneosmar89@gmail.com>
2019-02-06 19:57:55 -0300

    Remove RequestOption of the Glide

--- app/src/main/java/com/vortigo/pokemonfinder/helper/LoadImage.kt
+++ app/src/main/java/com/vortigo/pokemonfinder/helper/LoadImage.kt
@@ -2,8 +2,6 @@ package com.vortigo.pokemonfinder.helper
 
 import android.widget.ImageView
 import com.bumptech.glide.Glide
-import com.bumptech.glide.request.RequestOptions
-import com.vortigo.pokemonfinder.R
 
 /**
  * @author rorogarcete
@@ -14,13 +12,6 @@ import com.vortigo.pokemonfinder.R
 object LoadImage {
     @JvmStatic
     fun setImageUrl(imageView: ImageView, url: String?) {
-        val requestOptions = RequestOptions()
-        requestOptions.placeholder(R.drawable.poke_ball)
-        requestOptions.error(R.drawable.poke_ball)
-
-        Glide.with(imageView.context)
-            .setDefaultRequestOptions(requestOptions)
-            .load(url).into(imageView)
-
+        Glide.with(imageView.context).load(url).into(imageView)
     }
 }
