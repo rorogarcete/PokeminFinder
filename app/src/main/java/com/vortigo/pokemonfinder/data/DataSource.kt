63a4fc56748ff0026305a50bb5f4e239fd5e870c
rorogarcete <oneosmar89@gmail.com>
2019-02-07 00:08:19 -0300

    Refactor DataSource

--- app/src/main/java/com/vortigo/pokemonfinder/data/DataSource.kt
+++ app/src/main/java/com/vortigo/pokemonfinder/data/DataSource.kt
@@ -5,6 +5,12 @@ import com.vortigo.pokemonfinder.models.Trainer
 import com.vortigo.pokemonfinder.models.Type
 import io.reactivex.Observable
 
+/**
+ * @author rorogarcete
+ * @version 0.0.1
+ * DataSource interface for repository pattern
+ * Copyright 2019 Vortigo Inc. All rights reserved
+ */
 interface DataSource {
     fun getTypes(): Observable<List<Type>>
 
--- app/src/main/java/com/vortigo/pokemonfinder/ui/base/BaseFragment.kt
+++ app/src/main/java/com/vortigo/pokemonfinder/ui/base/BaseFragment.kt
@@ -5,7 +5,6 @@ import android.content.Intent
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.WindowManager
-
 import com.vortigo.pokemonfinder.R
 
 /**
